6a32f6337c3a62274b00e336fd7e297c625e5b9c
Benjamin <bhillenbrand101@gmail.com>
2018-10-01 16:46:55 -0500

    Added a create table statement

--- src/main/java/DB/encyclopediaDB.java
+++ src/main/java/DB/encyclopediaDB.java
@@ -1,26 +1,61 @@
 package DB;
 
-import java.sql.Connection;
+
+import java.sql.Connection;  // could use import java.sql.*; for everything
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.sql.Statement;
 
+import java.sql.ResultSet; // for returning data, I believe
+
 public class encyclopediaDB {
 
-    public static void main(String[] args) {
+    //static Connection conn = null;
+
+//    public static Connection connect() {
+//        try {
+//            Class.forName("org.sqlite.JDBC");
+//            conn = DriverManager.getConnection("jdbc:sqlite:test:db");
+//            System.out.print("DB connected");
+//            return conn;
+//        } catch (SQLException e) {
+//            e.printStackTrace(System.out);
+//            return null;
+//        }
+//    }
+
+    public static void ConnectAndCreateTable() {
+
+        // MAKE CONNECTION TO DATABASE
         Connection conn = null;
-        // TODO MAKE CONNECTION TO DATABASE
         try {
-            Class.forName("org.sqlite.JDBC");
-            conn = DriverManager.getConnection("jdbc:sqlite:test:db");
+            //Class.forName("org.sqlite.JDBC");
+            String url = "jdbc:sqlite:test:db";
+            conn = DriverManager.getConnection(url);
             System.out.print("DB connected");
 
+            // CREATE TABLE IF NOT EXIT
+            String tableName = "MyTable";  // Change table name here
+            String sqlCreateTable = "CREATE TABLE IF NOT EXISTS " + tableName
+                    + " (SearchWord  CHAR(40),"  // Todo Should Char be TEXT ????
+                    + "  TextInfo    CHAR(2000),"  // Increase Number here to hold more wiki data
+                    + "  Picture     BLOB )";
 
+            Statement statementCT = conn.createStatement();
+            statementCT.execute(sqlCreateTable);
 
-        } catch (Exception e) {
-            System.out.print(e);
+        } catch (SQLException e) {
+            System.out.print(e.getMessage());
+        }finally {
+            try {
+                if (conn != null) {
+                    conn.close();
+                }
+            } catch (SQLException ex) {
+                System.out.println(ex.getMessage());
+            }
         };
-        // TODO CREATE TABLE IF NOT EXIT
+
 
         // Extra QUERY THE DATABASE FOR SUBJECT INFORMATION
 
@@ -29,7 +64,6 @@ public class encyclopediaDB {
         // Extra DELETE SUBJECT INFORMATION FROM TABLE
     }
 
-    public void ConnectionOne() {
-
-    }
 }
+
+
