81a2d9a7a7a58f28484a139cbdb8c642b57ca94b
Benjamin <bhillenbrand101@gmail.com>
2018-10-01 11:00:07 -0500

    Working on DB connection

--- src/main/java/DB/encyclopediaDB.java
+++ src/main/java/DB/encyclopediaDB.java
@@ -1,11 +1,25 @@
 package DB;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+
 public class encyclopediaDB {
+
+
 
     public static void main(String[] args) {
 
+        Connection c = null;
         // TODO MAKE CONNECTION TO DATABASE
+        try {
+            Class.forName("org.sqlite.JDBC");
+            c = DriverManager.getConnection("jdbc:sqlite:test:db");
+            System.out.print("DB connected");
 
+
+        } catch (Exception e) {
+            System.out.print(e);
+        };
 
         // TODO CREATE TABLE IF NOT EXIT
 
