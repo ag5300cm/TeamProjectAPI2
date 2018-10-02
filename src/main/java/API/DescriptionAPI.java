171a742ff1a461caf874720d1433ceef4025e529
xong <xong@email.com>
2018-10-01 20:32:19 -0500

    Removed the main methods in the API classes

--- src/main/java/API/DescriptionAPI.java
+++ src/main/java/API/DescriptionAPI.java
@@ -2,13 +2,9 @@ package API;
 
 public class DescriptionAPI {
 
-    public static void main(String[] args) {
-
         // TODO REQUEST API URL AND API KEY TO Mediawiki
 
         // TODO GET API TO QUERY SUBJECT DESC AND GET BACK JSON FILE
 
         // TODO USE JSON FILE TO RETRIEVE SUBJECT DESC, SET DESC TEXT AREA TO DISPLAY SUBJECT DESC
-
-    }
 }
--- src/main/java/API/ImageAPI.java
+++ src/main/java/API/ImageAPI.java
@@ -2,12 +2,9 @@ package API;
 
 public class ImageAPI {
 
-    public static void main(String[] args) {
-
         // TODO REQUEST API URL AND API KEY TO Bing Image Search
 
         // TODO GET API TO QUERY SUBJECT IMAGE AND GET BACK JSON FILE
 
         // TODO USE JSON FILE TO RETRIEVE SUBJECT IMAGE, SET PICTURE LABEL TO DISPLAY SUBJECT IMAGE
-    }
 }
