431b645bdd901b54d7a706159673cfcc15381c54
xong <xong@email.com>
2018-10-02 16:05:54 -0500

    Updated TODO statements for API files

--- src/main/java/API/DescriptionAPI.java
+++ src/main/java/API/DescriptionAPI.java
@@ -4,17 +4,16 @@ public class DescriptionAPI {
 
     public static void main(String[] args) {
 
-        // TODO REQUEST API URL AND API KEY TO Mediawiki
+        // TODO MAKE API REQUEST URL TO Mediawiki
+
+        // https://en.wikipedia.org/w/api.php
+
+        // http://en.wikipedia.org/w/api.php?action=query&titles=Stackoverflow
+        // TODO GET BACK A JSON FILE
 
 
-//        https://en.wikipedia.org/w/api.php
 
-//
-        // TODO GET API TO QUERY SUBJECT DESC AND GET BACK JSON FILE
-
-
-
-        // TODO USE JSON FILE TO RETRIEVE SUBJECT DESC, SET DESC TEXT AREA TO DISPLAY SUBJECT DESC
+        // TODO USE JSON FILE TO RETRIEVE SUBJECT DESC
 
     }
 
