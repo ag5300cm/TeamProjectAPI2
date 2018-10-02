65c687acdaebe0c65212176124ee758bd6eb4ad8
xong <xong@email.com>
2018-10-02 15:35:34 -0500

    Add gitignore file

--- src/main/java/API/DescriptionAPI.java
+++ src/main/java/API/DescriptionAPI.java
@@ -2,9 +2,21 @@ package API;
 
 public class DescriptionAPI {
 
+    public static void main(String[] args) {
+
         // TODO REQUEST API URL AND API KEY TO Mediawiki
 
+
+//        https://en.wikipedia.org/w/api.php
+
+//        http://en.wikipedia.org/w/api.php?action=query&titles=Stackoverflow
         // TODO GET API TO QUERY SUBJECT DESC AND GET BACK JSON FILE
 
+
+
         // TODO USE JSON FILE TO RETRIEVE SUBJECT DESC, SET DESC TEXT AREA TO DISPLAY SUBJECT DESC
+
+    }
+
+
 }
