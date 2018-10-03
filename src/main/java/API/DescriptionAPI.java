58365dea7fc8c82ee41244f97518afd50fab2f74
xong <xong@email.com>
2018-10-03 15:49:46 -0500

    Connected to WIKI api using httpurlconnection and retrieved json

--- src/main/java/API/DescriptionAPI.java
+++ src/main/java/API/DescriptionAPI.java
@@ -1,21 +1,58 @@
 package API;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import org.json.JSONObject;
+
 public class DescriptionAPI {
 
     public static void main(String[] args) {
 
-        // TODO MAKE API REQUEST URL TO Mediawiki
+        try {
+            String searchText = "candy";
 
-        // https://en.wikipedia.org/w/api.php
+            String urlString = "https://en.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&exintro=&explaintext=&titles=" + searchText;
 
-        // http://en.wikipedia.org/w/api.php?action=query&titles=Stackoverflow
-        // TODO GET BACK A JSON OBJECT
+            URL urlObject = new URL(urlString);
 
+            HttpURLConnection con = (HttpURLConnection) urlObject.openConnection();
 
+            con.setRequestMethod("GET");
 
-        // TODO USE JSON TO RETRIEVE SUBJECT DESC
+            int responseCode = con.getResponseCode();
+
+            if(responseCode == 200 || responseCode == 201) {
+
+                BufferedReader in = new BufferedReader(
+                        new InputStreamReader(con.getInputStream()));
+
+                String inputLine;
+
+                StringBuffer response = new StringBuffer();
+
+                while ((inputLine = in.readLine()) != null) {
+                    response.append(inputLine);
+                }
+
+                //System.out.println(response.toString());
+
+                // Read JSON response and retrieve subject's description
+                JSONObject json = new JSONObject(response.toString());
+                System.out.println("Description: " + json.getJSONObject("query").getJSONObject("pages").getJSONObject("61230").getString("extract"));
+
+                in.close();
+            }
+
+            con.disconnect();
+
+        } catch (java.net.MalformedURLException me) {
+            System.out.println("Error " + me);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
 
     }
-
 
 }
